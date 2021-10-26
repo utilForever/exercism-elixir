6b3269a75638572e28da781332f8c9cd39dcb897
Chris Ohk <utilForever@gmail.com>
2021-10-26 14:18:29 +0900

    Upload exercise "Freelancer Rates"

---
+++ freelancer_rates.ex
@@ -0,0 +1,17 @@
+defmodule FreelancerRates do
+  def daily_rate(hourly_rate) do
+    8.0 * hourly_rate
+  end
+
+  def apply_discount(before_discount, discount) do
+    before_discount * (1.0 - discount / 100)
+  end
+
+  def monthly_rate(hourly_rate, discount) do
+    ceil(apply_discount(22 * daily_rate(hourly_rate), discount))
+  end
+
+  def days_in_budget(budget, hourly_rate, discount) do
+    Float.floor(budget / apply_discount(daily_rate(hourly_rate), discount), 1)
+  end
+end
