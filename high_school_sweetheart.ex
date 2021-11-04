92241d7650226edf417c8b74f9cc54ea5cc8c505
Chris Ohk <utilForever@gmail.com>
2021-11-04 12:49:15 +0900

    Upload exercise "High School Sweetheart"

---
+++ high_school_sweetheart.ex
@@ -0,0 +1,32 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    String.trim(name) |> String.first()
+  end
+
+  def initial(name) do
+    first_letter(name) |> String.upcase() |> Kernel.<>(".")
+  end
+
+  def initials(full_name) do
+    String.split(full_name) |> Enum.map(fn name -> initial(name) end) |> Enum.join(" ")
+  end
+
+  def pair(full_name1, full_name2) do
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{initials(full_name1)}  +  #{initials(full_name2)}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
+  end
+end
